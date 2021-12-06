0d402dbf80e449dcfb7c73acec0c360b0324d1e2
JamesJohnson <johnson.softs111@gmail.com>
2021-12-06 13:06:28 +0500

    database changed

--- database/connection.js
+++ database/connection.js
@@ -2,13 +2,22 @@ const mysql = require('mysql');
 
 const db = mysql.createPool( 
     {
-        host: 'localhost',
+        // host: 'localhost',
+        // port : '3306',
+        // user: 'root',
+        // password: '',
+        // database: 'student-info',
+        // multipleStatements: true,
+        // supportBigNumbers: true
+
+
+        host: 'remotemysql.com',
         port : '3306',
-        user: 'root',
-        password: '',
-        database: 'student-info',
+        user: 'HVCcBMfMED',
+        password: 'JgQVsIZ8mY',
+        database: 'HVCcBMfMED',
         multipleStatements: true,
-        supportBigNumbers: true
+        supportBigNumbers: true,
     }
 );
 
