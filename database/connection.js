62a140d0347d201a2fd65075e0c15d33b91f2642
USMAN ALALEMY <johnson.softs111@gmail.com>
2022-01-23 11:37:40 +0500

    New Student Porstal With Dashbaord

--- database/connection.js
+++ database/connection.js
@@ -2,21 +2,21 @@ const mysql = require('mysql');
 
 const db = mysql.createPool( 
     {
-        host: 'localhost',
-        port : '3306',
-        user: 'root',
-        password: '',
-        database: 'student-info',
-        multipleStatements: true,
-        supportBigNumbers: true
-
-        // host: 'remotemysql.com',
+        // host: 'localhost',
         // port : '3306',
-        // user: 'HVCcBMfMED',
-        // password: 'JgQVsIZ8mY',
-        // database: 'HVCcBMfMED',
+        // user: 'root',
+        // password: '',
+        // database: 'student-info',
         // multipleStatements: true,
-        // supportBigNumbers: true,
+        // supportBigNumbers: true
+
+        host: 'remotemysql.com',
+        port : '3306',
+        user: 'HVCcBMfMED',
+        password: 'JgQVsIZ8mY',
+        database: 'HVCcBMfMED',
+        multipleStatements: true,
+        supportBigNumbers: true,
     }
 );
 
